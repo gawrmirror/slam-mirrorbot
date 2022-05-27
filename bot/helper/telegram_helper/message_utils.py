cb652db6244afbd12d9fcbd1653f39b572ba7fe1
gawrmirror <105641633+gawrmirror@users.noreply.github.com>
2022-05-27 15:46:29 +0700

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -13,19 +13,19 @@ def sendMessage(text: str, bot, update: Update):
     try:
         return bot.send_message(update.message.chat_id,
                             reply_to_message_id=update.message.message_id,
-                            text=text, allow_sending_without_reply=True,  parse_mode='markdown')
+                            text=text, allow_sending_without_reply=True,  parse_mode='HTML')
     except Exception as e:
         LOGGER.error(str(e))
 def sendMarkup(text: str, bot, update: Update, reply_markup: InlineKeyboardMarkup):
     return bot.send_message(update.message.chat_id,
                             reply_to_message_id=update.message.message_id,
-                            text=text, reply_markup=reply_markup, allow_sending_without_reply=True, parse_mode='markdown')
+                            text=text, reply_markup=reply_markup, allow_sending_without_reply=True, parse_mode='HTML')
 
 def editMessage(text: str, message: Message, reply_markup=None):
     try:
         bot.edit_message_text(text=text, message_id=message.message_id,
                               chat_id=message.chat.id,reply_markup=reply_markup,
-                              parse_mode='markdown')
+                              parse_mode='HTML')
     except Exception as e:
         LOGGER.error(str(e))
 
